6eb47d18f739b6b47d30113d8304a9f0b46ea517
Dzakagraha <dzakisn45@gmail.com>
2016-11-07 15:55:53 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/MainActivity.java
@@ -0,0 +1,35 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.learn.recyclerview1.adapter.HotelAdapter;
+import id.sch.smktelkom_mlg.learn.recyclerview1.model.Hotel;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Hotel> mList = new ArrayList<>();
+    HotelAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+    }
+
+    private void fillData() {
+
+    }
+}
